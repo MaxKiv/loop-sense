b6951e20b94a869e14f3f0a17529d106b2c246f9
MaxKiv <maxkivits42@gmail.com>
2025-07-14 16:47:19 +0200

    feat(db): updated db snapshot to include more test data

--- src/database/secrets.rs
+++ src/database/secrets.rs
@@ -2,5 +2,11 @@
 pub const DB_URI: &str = "http://localhost:8181";
 pub const DB_NAME: &str = "mockloop_data";
 pub const MEASUREMENT_ID_TABLE: &str = "measurement_id";
+
+// NOTE: this is the memory db token
+// pub const DB_ACCESS_TOKEN: &str =
+//     "apiv3_5zB9k-A7Eora5iMy3epTdWi6NjaRzTvF2jx1mprt98l2z4eOl2tyZTLdnjHzzmqB4kwD_z681ynKVaSXf4Lvcw";
+
+// NOTE: this is the docker db token
 pub const DB_ACCESS_TOKEN: &str =
-    "apiv3_5zB9k-A7Eora5iMy3epTdWi6NjaRzTvF2jx1mprt98l2z4eOl2tyZTLdnjHzzmqB4kwD_z681ynKVaSXf4Lvcw";
+    "apiv3_Sjv0jhXxm8IeDy61CgX1J9KTpjltr1-JKiJv_xsSh0XEuDLTV9pPP9TV4P9G5OoXl3uLqIaOQsruioawRfFbog";
