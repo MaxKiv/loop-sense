c1a8c46bbb5631b4ed16c1a8674b43cd69e03999
kousheekc <k.chakraborty@saxion.nl>
2026-03-03 16:54:13 +0100

    dhcp

--- nixos/rpi4/network.nix
+++ nixos/rpi4/network.nix
@@ -14,6 +14,8 @@
   # Prevent host becoming unreachable on wifi after some time
   networking.networkmanager.wifi.powersave = false;
 
+  networking.networkmanager.unmanaged = [ "interface-name:end0" ];
+
   networking.interfaces.end0.ipv4.addresses = [
     {
       address = "192.168.0.4";
@@ -21,10 +23,30 @@
     }
   ];
 
+  # DHCP server for the laptop on end0
+  services.dnsmasq = {
+    enable = true;
+    settings = {
+      interface = "end0";
+      bind-interfaces = true;
+
+      # Hand out addresses in same subnet, excluding .4
+      dhcp-range = "192.168.0.50,192.168.0.150,255.255.255.0,12h";
+
+      # Optional: tell clients the gateway is the Pi (useful if you later enable NAT)
+      dhcp-option = [
+        "option:router,192.168.0.4"
+        # Optional DNS advertised to client:
+        # "option:dns-server,192.168.0.4"
+      ];
+    };
+  };
+
   networking.firewall = {
     enable = true;
     checkReversePath = false;
     allowedTCPPorts = [8000 8086 5173 80];
-    allowedUDPPorts = [8000 8086];
+    allowedUDPPorts = [67 8000 8086];
+    # allowedUDPPorts = [67 53 8000 8086];
   };
 }
